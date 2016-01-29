998abe475b60b7b0527557355fb565fe71109b3e
Kai Sickeler <k.sickeler@gmail.com>
2016-01-29 11:03:13 +0100

    cli-args are working now

--- src/decode_rss.rs
+++ src/decode_rss.rs
@@ -4,8 +4,8 @@ use rss::Rss;
 use html_handler;
 
 pub fn print_titles(feed: rss::Channel, start: usize,  n: usize) {
-    for title in feed.items.into_iter().skip(start).take(n).map(|item| item.title) {
-        title.map(|t| println!("{}", t))
+    for (pos, title) in feed.items.into_iter().skip(start).take(n).map(|item| item.title).enumerate() {
+        title.map(|t| println!("{}\t{}", pos + start,  t))
             .or_else(|| panic!("no valid data found"));
     }
 }
@@ -22,4 +22,5 @@ pub fn print_article(feed: rss::Channel, id: usize) {
     print_titles(feed.clone(), id, 1);
     println!("===============================================================");
     print_descriptions(feed.clone(), id, 1);
+    println!("---------------------------------------------------------------\n")
 }
--- src/main.rs
+++ src/main.rs
@@ -28,10 +28,9 @@ fn main() {
 
     //fetch all arguments
     let url = param.value_of("url").unwrap();
-    let title_only = value_t!(param.value_of("title_only"), bool).unwrap_or(false);
-    let list =  value_t!(param.value_of("title_only"), bool).unwrap_or(false);
-    let num = value_t!(param.value_of("num"), isize).unwrap_or(-1);
-    let start = value_t!(param.value_of("num"), isize).unwrap_or(-1);
+    let title_only = param.is_present("title-only");
+    let num = value_t!(param.value_of("num"), usize).unwrap_or(1);
+    let start = value_t!(param.value_of("start"), usize).unwrap_or(0);
 
     // fetch the url and get the rss string
     let content = html_handler::get_rss_feed(&url); 
@@ -39,7 +38,15 @@ fn main() {
     //parse that string into rss data
     let rss = Rss::from_str(&content).unwrap_or_else(|err| exit("URL's content could not be fetched") ); 
     let Rss(feed) = rss.clone(); // get the parsed content
-    decode_rss::print_article(feed, 1);
+
+    if title_only == true {
+        decode_rss::print_titles( feed.clone() , start, num);
+        return
+    }
+
+    for pos in start..start+num{
+        decode_rss::print_article(feed.clone(), pos);
+    }
 }
 
 fn exit(err: &str) -> ! {
